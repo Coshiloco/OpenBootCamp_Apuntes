b0e670730cb7c54f4fc1c53f2f4d4295ab54d334
Samuel-Galindo <samuelgalindop@gmail.com>
2022-06-09 13:18:38 +0200

    Python Tema 5 Ejercicios

---
+++ 02-Python/Exercises/ejercicio-5.3.py
@@ -0,0 +1,12 @@
+def bisiesto(year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        print(year, "Es bisiesto")
+    else:
+        print(year, "No es bisiesto")
+
+
+bisiesto(845)
+bisiesto(896)
+bisiesto(963)
+bisiesto(2400)
+
