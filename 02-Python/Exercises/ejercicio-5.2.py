1927ad2fcbff3d72354deb851e296ade303a374b
Samuel-Galindo <samuelgalindop@gmail.com>
2022-06-09 12:59:46 +0200

    Python Tema 5 Ejercicios

---
+++ 02-Python/Exercises/ejercicio-5.2.py
@@ -0,0 +1,18 @@
+def esPrimo(num):
+    if type(num) == float:
+        print("No es un numero entero")
+        return False
+
+    for n in range(2, num):
+        if num % n == 0:
+            print(num, "No es primo", n, "es divisor")
+            return False
+    print(num, "Es primo")
+    return True
+
+
+esPrimo(5.6)
+esPrimo(11)
+esPrimo(89)
+esPrimo(567)
+esPrimo(1001)
