6796f00a6bf5be724e398c104fc8ec92cde79522
Samuel-Galindo <samuelgalindop@gmail.com>
2022-06-09 12:46:29 +0200

    Python Tema 5 Ejercicios

---
+++ 02-Python/Exercises/ejercicio-5.1.py
@@ -0,0 +1,13 @@
+def areatriangulo(altura, base):
+    area = altura * base / 2
+    print(area)
+
+
+def areacirculo(radio):
+    pi = 3.1415
+    area = pi * radio**2
+    print(round(area, 2))
+
+
+areatriangulo(6, 3)
+areacirculo(23)
