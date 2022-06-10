44c03812ca338d467f66a8847660f4f2a7fc13d1
Samuel-Galindo <samuelgalindop@gmail.com>
2022-06-10 17:33:55 +0200

    Tema 7 + Ejercicios

---
+++ 02-Python/Exercises/ejercicio-7.1.py
@@ -0,0 +1,6 @@
+import modulo7 as maths
+
+print(maths.sumar(4, 5))
+print(maths.restar(6, 10))
+print(maths.multiplicar(4, 6))
+print(maths.dividir(2, 8))
---
+++ 02-Python/Exercises/modulo7.py
@@ -0,0 +1,14 @@
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    return a / b
